0661f72585745df8ba09383fe839077386d1e2a5
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2023-08-31 00:12:56 +0700

    Update js_index.js

--- assets/js/js_index.js
+++ assets/js/js_index.js
@@ -57,11 +57,17 @@ increaseVisitCount();
 
 
 
-// document.addEventListener("contextmenu", function(e) {
-//     e.preventDefault();
-// });
-// document.addEventListener("keydown", function(e) {
-//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    alert('>> Tokisaki Nino: "Say no to "Inspect" website!" :))');
+});
+  
