bb3b8cc74307bb6cb820248e30fd9ad1fdb34429
TokisakiNinoVn <trieuduytancbg@gmail.com>
2023-09-08 15:20:19 +0700

    New update

--- assets/js/js_index.js
+++ assets/js/js_index.js
@@ -53,33 +53,33 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 increaseVisitCount();
 
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
+// document.addEventListener("contextmenu", function(e) {
+//     e.preventDefault();
+// });
 
 
 
 
 // Ban F12
-document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
-        e.preventDefault();
-    }
-});
+// document.addEventListener("keydown", function(e) {
+//     if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+//         e.preventDefault();
+//     }
+// });
 
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-    alert('>> Tokisaki Nino: "Say no to "Inspect" website!" :))');
-});
+// document.addEventListener('contextmenu', function(event) {
+//     event.preventDefault();
+//     alert('>> Tokisaki Nino: "Say no to "Inspect" website!" :))');
+// });
 
-window.addEventListener('keydown', function(event) {
-    if (event.key === 'F12' || event.keyCode === 123) {
-    //   alert('Không được phép truy cập mã nguồn trang web!');
-      alert('Access to website source code is not allowed!');
+// window.addEventListener('keydown', function(event) {
+//     if (event.key === 'F12' || event.keyCode === 123) {
+//     //   alert('Không được phép truy cập mã nguồn trang web!');
+//       alert('Access to website source code is not allowed!');
 
-      event.preventDefault();
-    }
-  });
+//       event.preventDefault();
+//     }
+//   });
 
   
 
