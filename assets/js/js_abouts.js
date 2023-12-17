173f6c67caf54b4ea33c436eb61a288e49f07a4e
tokisakininovn <trieuduytancbg@gmail.com>
2023-12-17 18:33:13 +0700

    New Update

---
+++ assets/js/js_abouts.js
@@ -0,0 +1,11 @@
+let infor_posts = [
+    {
+        tittle: "Tokisaki Nino",
+        urlImg: "https://nino.is-a.dev/",
+        dayPost: "2021/08/01",
+        linkSource: "https://nino.is-a.dev/",
+        countView: "0",
+    },
+
+
+]
