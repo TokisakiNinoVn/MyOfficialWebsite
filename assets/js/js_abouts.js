7b24f4a07f1778dfe88bb5251ac545938d9e700b
tokisakininovn <trieuduytancbg@gmail.com>
2023-12-17 20:20:15 +0700

    New Update

--- assets/js/js_abouts.js
+++ assets/js/js_abouts.js
@@ -1,6 +1,7 @@
 let infor_posts = [
     {
         tittle: "Tokisaki Nino",
+        description: "Tokisaki Nino (時崎 狂三, Tokisaki Kurumi) is a third-year student at Asahigaoka Branch School. She is the older sister of Tokisaki Miku and Tokisaki Yotsuba. She is also the leader of the band in the school.",
         urlImg: "https://nino.is-a.dev/",
         dayPost: "2021/08/01",
         linkSource: "https://nino.is-a.dev/",
