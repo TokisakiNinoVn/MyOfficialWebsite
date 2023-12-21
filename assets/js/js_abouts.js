3aeac08853d4c0884fc9f64dc9cbeb52a8b09c29
tokisakininovn <trieuduytancbg@gmail.com>
2023-12-21 23:35:04 +0700

    New Update

--- assets/js/js_abouts.js
+++ assets/js/js_abouts.js
@@ -24,47 +24,6 @@ function generateRandomNumber() {
 }
 setInterval(generateRandomNumber, 2000); // Chuyển màu sau mỗi 2 giây
 
-
-//Like icon ___________________________________________________________________________
-const likeBtn_Click = document.getElementById("like_like");
-const likeIcon_false = document.querySelector(".like_false");
-const likeIcon_true = document.querySelector(".like_true");
-likeBtn_Click.addEventListener("click", function () {
-    if (likeIcon_false.classList.contains("active")) {
-        likeIcon_false.classList.remove("active");
-        likeIcon_true.classList.add("active");
-    } else {
-        likeIcon_false.classList.add("active");
-        likeIcon_true.classList.remove("active");
-    }
-});
-
-//Display basic information or Posts ___________________________________________________________________________
-const basicInformation_Click = document.getElementById("basic_status");
-const posts_Click = document.getElementById("post_status");
-const mainInformation = document.querySelector(".basic_infor");
-const mainPosts = document.querySelector(".posts");
-const bannerElysia = document.querySelector(".img_banner_elysia");
-
-document.addEventListener("DOMContentLoaded", function () { 
-    basicInformation_Click.addEventListener("click", function () {
-        mainInformation.classList.add("active");
-        basicInformation_Click.classList.add("active_");
-        mainPosts.classList.remove("active");
-        posts_Click.classList.remove("active_");
-    });
-    
-    posts_Click.addEventListener("click", function () {
-        mainInformation.classList.remove("active");
-        basicInformation_Click.classList.remove("active_");
-        mainPosts.classList.add("active");
-        posts_Click.classList.add("active_");
-    });
-});
-
-
-
-
 // async function fetchDiscordStatus() {
 //     try {
 //         const response = await axios.get(
@@ -103,3 +62,115 @@ document.addEventListener("DOMContentLoaded", function () {
 // Gọi hàm để lấy và cập nhật trạng thái Discord
 // fetchDiscordStatus();
 // setInterval(fetchDiscordStatus, 1000);  // Interval là 1 giây, không cần gọi mỗi giây
+
+
+//Like icon > play audio___________________________________________________________________________
+document.addEventListener("DOMContentLoaded", function () {
+    const likeBtn_Click = document.getElementById("like_like");
+    const likeIcon_false = document.querySelector(".like_false");
+    const likeIcon_true = document.querySelector(".like_true");
+    const tool_music = document.querySelector(".tool_ely");
+
+    const play_audio = document.querySelector(".play_icon");
+    const pause_audio = document.querySelector(".pause_icon");
+    const control_audio = document.querySelector(".control_play");
+
+    const controlVolume = document.querySelector(".control_volume");
+    const muteVolumeTrue = document.querySelector(".mute_volume-true");
+    const muteVolumeFalse = document.querySelector(".mute_volume-false");
+    
+    const true_audioLink = new Audio("./assets/audio/True.mp3");
+    var like_status = false;
+    var audio_status = false;
+    var volume_status = false;
+
+
+    const basicInformation_Click = document.getElementById("basic_status");
+    const posts_Click = document.getElementById("post_status");
+    const mainInformation = document.querySelector(".basic_infor");
+    const mainPosts = document.querySelector(".posts");
+    const bannerElysia = document.querySelector(".img_banner_elysia");
+    
+    //Display basic information or Posts ___________________________________________________________________________
+    basicInformation_Click.addEventListener("click", function () {
+        mainInformation.classList.add("active");
+        basicInformation_Click.classList.add("active_");
+        mainPosts.classList.remove("active");
+        posts_Click.classList.remove("active_");
+        if (like_status == true) {
+            bannerElysia.classList.add("active");
+        }
+    });
+    
+    posts_Click.addEventListener("click", function () {
+        mainInformation.classList.remove("active");
+        basicInformation_Click.classList.remove("active_");
+        mainPosts.classList.add("active");
+        posts_Click.classList.add("active_");
+
+        if(bannerElysia.classList.contains("active")){
+            bannerElysia.classList.remove("active");
+        }
+    });
+    
+    likeBtn_Click.addEventListener("click", function () {
+        if (likeIcon_false.classList.contains("active")) {
+            likeIcon_false.classList.remove("active");
+            likeIcon_true.classList.add("active");
+            bannerElysia.classList.add("active");
+            like_status = true;
+            
+            true_audioLink.play()
+            audio_status = true;
+            true_audioLink.volume = 0.1;
+            tool_music.classList.add("tool_ely_active");
+            play_audio.classList.remove("active");
+            pause_audio.classList.add("active");
+            volume_status = true;
+        } else {
+            likeIcon_false.classList.add("active");
+            likeIcon_true.classList.remove("active");
+            bannerElysia.classList.remove("active");
+            like_status = false;
+
+            pauseAudio();
+        }
+    });
+    
+
+    control_audio.addEventListener("click", function () {
+        if (audio_status == false) {
+            playAudio();
+            audio_status = true;
+        } else {
+            pauseAudio();
+            audio_status = false;
+        }
+    });
+
+    controlVolume.addEventListener("click", function () {
+        if (volume_status == true) {
+            true_audioLink.volume = 0;
+            muteVolumeTrue.classList.add("active");
+            muteVolumeFalse.classList.remove("active");
+            volume_status = false;
+        } else {
+            true_audioLink.volume = 0.1;
+            muteVolumeTrue.classList.remove("active");
+            muteVolumeFalse.classList.add("active");
+            volume_status = true;
+        }
+    });
+
+    function playAudio() {
+        true_audioLink.play();
+        play_audio.classList.remove("active");
+        pause_audio.classList.add("active");
+    }
+
+    function pauseAudio() {
+        true_audioLink.pause();
+        play_audio.classList.add("active");
+        pause_audio.classList.remove("active");
+    }
+});
