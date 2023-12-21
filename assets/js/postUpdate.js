3c33c88d805a8cbf57dd3ab02f20d33adb4530ad
tokisakininovn <trieuduytancbg@gmail.com>
2023-12-21 19:36:44 +0700

    New Update

--- assets/js/postUpdate.js
+++ assets/js/postUpdate.js
@@ -2,6 +2,7 @@
 /* <b></b> */
 /* <br> */
 /* <i></i> */
+// style="height: 100px;"
 
 const html_ = `<a href="https://www.pinterest.com/ninomemories/elysia/" target="_blank">🍀Here.</a>`
 const iwe = ` 🪶`
@@ -9,7 +10,7 @@ const linkNino = "https://nino.is-a.dev/"
 let infor_posts = [
     {
         tittle: "Christmas wishes 2023!",
-        description: "Merry Christmas 2023! During this holiday season, I hope you and your family will be filled with joy, happiness and warmth. Wishing everyone a holiday filled with laughter, friendship, and memorable memories. Wishing you a new year filled with health, luck and success. May your dreams come true, and every step in life brings happiness and success. This year, keep your heart warm and full of love. Share joy with those around you and create meaningful moments. Christmas is not only a time to receive gifts, but also an opportunity to share, do charity and beautify the world around us. Happy Christmas and New Year, hope all the best things will come to you and your family. Merry Christmas and Happy New Year 2023! 🎄🌟",
+        description: "<b>Merry Christmas 2023!</b> During this holiday season, I hope you and your family will be filled with joy, happiness and warmth. Wishing everyone a holiday filled with laughter, friendship, and memorable memories. Wishing you a new year filled with health, luck and success. May your dreams come true, and every step in life brings happiness and success. This year, keep your heart warm and full of love. Share joy with those around you and create meaningful moments. Christmas is not only a time to receive gifts, but also an opportunity to share, do charity and beautify the world around us. Happy Christmas and New Year, hope all the best things will come to you and your family. Merry Christmas and Happy New Year 2023! 🎄🌟",
         urlImg: "https://pbs.twimg.com/media/GA4VuctasAA3j2U?format=jpg&name=large",
         dayPost: "2023/12/24" + iwe,
         countView: "0",
