3781dd9a246660bc2287b0abc2b311bc0b6e5c6f
Nino <110331442+TokisakiNinoVn@users.noreply.github.com>
2024-01-22 23:51:30 +0700

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -24,6 +24,12 @@ let infor_project = [
         img: "./assets/image/Ai.jpg",
         name: "Honkai Star Rail Loading HTML-CSS",
     },
+        {
+        url: "https://nino.is-a.dev/FreeStickersHoyolab/",
+        img: "./assets/image/Ai.jpg",
+        name: "Free sticker Hoyolab to use Discord",
+    },
+
 
 ]
 
