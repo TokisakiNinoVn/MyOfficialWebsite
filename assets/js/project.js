78aa2424286ac21a954db3d07515d909aee55e89
tokisakininovn <trieuduytancbg@gmail.com>
2024-02-23 00:56:09 +0700

    wmj

--- assets/js/project.js
+++ assets/js/project.js
@@ -10,29 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             showProjectEvenNumber(project);
         }
-        // const listProjectDiv = document.createElement("div");
-        // listProjectDiv.classList.add("item_project");
-
-        // const img = document.createElement("img");
-        // img.src = project.img;
-        // img.alt = "";
-        // img.classList.add("preview");
-
-        // const divLink = document.createElement("div");
-        // divLink.classList.add("div_link");
-
-        // const a = document.createElement("a");
-        // a.href = project.linkView;
-        // a.classList.add("link_project");
-
-        // const span = document.createElement("span");
-        // span.textContent = project.name;
-
-        // a.appendChild(span);
-        // divLink.appendChild(a);
-        // listProjectDiv.appendChild(img);
-        // listProjectDiv.appendChild(divLink);
-        // projectDiv.appendChild(listProjectDiv);
     }
 });
 
@@ -40,7 +17,6 @@ function flagNumber(value) {
     if (value%2 == 1) return true;
     else return false;
 }
-
 
 function showProjectEvenNumber(infor_project) {
     const html = `
