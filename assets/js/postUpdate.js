0a43aca993ea8456b53779ba7511ed7c90cdc271
tokisakininovn <trieuduytancbg@gmail.com>
2024-02-22 14:49:07 +0700

    wkqd

--- assets/js/postUpdate.js
+++ assets/js/postUpdate.js
@@ -5,9 +5,19 @@
 // style="height: 100px;"
 
 const html_ = `<a href="https://www.pinterest.com/ninomemories/elysia/" target="_blank">🍀Here.</a>`
-const iwe = ` 🪶`
+const iwe = `🪶`
 const linkNino = "https://nino.is-a.dev/"
 let infor_posts = [
+    {
+        tittle: "Happy New Year 2024! 🎉🎉🎉",
+        description: "Wishing everyone a new year filled with happiness, good health and great success! Hope the best things will come to you and your family in this new year. Leave the burdens of the old year behind and welcome new opportunities, new experiences and new achievements in the coming new year.        Happy New Year, live strong, love and be patient in the face of all challenges. Enjoy every moment and make every day a memorable one! A peaceful and happy new year!",
+        urlImg: "https://pbs.twimg.com/media/GECXTnRbYAALhui?format=jpg&name=large",
+        dayPost: "2023/12/24" + iwe,
+        countView: "0",
+        linkSource: linkNino,
+        hashTag: "#TokisakiNino, #Nino, #Elysia, #SimpForElysia, #HappyNewYear2024",
+        viewCount: Math.floor(Math.random() * 2000),
+    },
     {
         tittle: "Christmas wishes 2023!",
         description: "<b>Merry Christmas 2023!</b> During this holiday season, I hope you and your family will be filled with joy, happiness and warmth. Wishing everyone a holiday filled with laughter, friendship, and memorable memories. Wishing you a new year filled with health, luck and success. May your dreams come true, and every step in life brings happiness and success. This year, keep your heart warm and full of love. Share joy with those around you and create meaningful moments. Christmas is not only a time to receive gifts, but also an opportunity to share, do charity and beautify the world around us. Happy Christmas and New Year, hope all the best things will come to you and your family. Merry Christmas and Happy New Year 2024! 🎄🌟",
