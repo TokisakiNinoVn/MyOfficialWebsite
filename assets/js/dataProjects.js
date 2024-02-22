6ce71b02a54651fe2c1ac9b3e34c4ecad4c1100c
tokisakininovn <trieuduytancbg@gmail.com>
2024-02-23 00:28:10 +0700

    acac

--- assets/js/dataProjects.js
+++ assets/js/dataProjects.js
@@ -1,14 +1,14 @@
 const infor_project = [
     {
         flagNumber: 2,
-        linkView: "https://nino.is-a.dev/", //1
-        img: "../assets/image/HoshinoAi/_horizontal/HoshinoAi(1).jpg", //2
-        dateDev: "2023-11-02", // 3
-        source: "", //4
-        name: "My Card Information", //5
-        description: "Hihi", // 6
-        language: "HTML, CSS, JS", //7
-        collection: Math.floor(Math.random() * 20), //8
+        linkView: "https://nino.is-a.dev/",
+        img: "../assets/image/HoshinoAi/_horizontal/HoshinoAi(1).jpg",
+        dateDev: "2023-11-02", 
+        source: "",
+        name: "My Card Information",
+        description: "Hihi", 
+        language: "HTML, CSS, JS",
+        collection: Math.floor(Math.random() * 20),
     },
     {
         flagNumber: 3,
@@ -35,7 +35,7 @@ const infor_project = [
     {
         flagNumber: 3,
         linkView: "https://nino.is-a.dev/CharacterChampionsLeague/",
-        img: "./assets/image/HoshinoAi/_horizontal/HoshinoAi(4).jpg",
+        img: "../assets/image/HoshinoAi/_horizontal/HoshinoAi(4).jpg",
         dateDev: "2023-11-24",
         source: "#",
         name: "Character Champions League",
